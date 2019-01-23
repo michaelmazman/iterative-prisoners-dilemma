ed93fba5f88e007e8b4507e1764ce5acdd040a3e
michaelmazman <46687439+michaelmazman@users.noreply.github.com>
2019-01-23 10:51:57 -0800

    Update teammm.py

--- teammm.py
+++ teammm.py
@@ -24,26 +24,25 @@ def move(my_history, their_history, my_score, their_score):
     
     Returns 'c' or 'b' for collude or betray.
     '''
-    if len(my_history)==0: # It's the first round: collude
-        return 'c'
-    else:
-        # If there was a previous round just like the last one,
-        # do whatever they did in the round that followed it
-        
-        # Reference last round
-        recent_round_them = their_history[-1]
-        recent_round_me = my_history[-1]
-                    
-        # Look at rounds before that one
-        for round in range(len(my_history)-1):
-            prior_round_them = their_history[round]
-            prior_round_me = my_history[round]
-            # If one matches
-            if (prior_round_me == recent_round_me) and \
-                    (prior_round_them == recent_round_them):
-                return their_history[round]
-        # No match found
-        if my_history[-1]=='c' and their_history[-1]=='b':
-            return 'b' # Betray if they were severely punished last time
-        else:
-            return 'c' # Otherwise collude.
+   #1 = collude
+   #0 = defect
+
+beNice = True
+
+def play(opponentMove):
+  opponentHistory= []
+  global beNice
+  opponentHistory.append(opponentMove)
+  if opponentMove == 'start':
+    beNice = True
+    opponentHistory.append(1)
+    return 1
+  if opponentHistory[-1] == 0: #this refers to the last move
+    beNice = False
+  if beNice==True:
+    return 1
+  else:
+    return 0
+
+def name():
+  return 'Michael'
